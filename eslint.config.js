5101e3f323f215d86c35acb5fad480fcc885cb16
danyaITi <danil.lenskiy.1997@mail.ru>
2025-01-13 19:16:39 +0300

    added prettier and eslint base rules

--- eslint.config.js
+++ eslint.config.js
@@ -1,15 +1,16 @@
 /** @format */
 
-import globals from "globals";
-import reactHooks from "eslint-plugin-react-hooks";
-import reactRefresh from "eslint-plugin-react-refresh";
-import tsParser from "@typescript-eslint/parser"; // Добавлен TypeScript парсер
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tsParser from '@typescript-eslint/parser';
+import unusedImports from 'eslint-plugin-unused-imports';
 
 export default [
   {
-    ignores: ["dist"],
+    ignores: ['dist'],
 
-    files: ["**/*.{ts,tsx}"],
+    files: ['**/*.{ts,tsx}'],
 
     languageOptions: {
       ecmaVersion: 2020,
@@ -18,17 +19,27 @@ export default [
     },
 
     plugins: {
-      "react-hooks": reactHooks,
-      "react-refresh": reactRefresh,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+      'unused-imports': unusedImports,
     },
 
     rules: {
       ...reactHooks.configs.recommended.rules,
-      "react-refresh/only-export-components": [
-        "warn",
-        { allowConstantExport: true },
+      'react-refresh/only-export-components': ['warn', {allowConstantExport: true}],
+      'no-unused-vars': 'warn',
+      'no-console': 'error',
+      'unused-imports/no-unused-imports': 'error',
+      'unused-imports/no-unused-vars': [
+        'warn',
+        {
+          vars: 'all',
+          varsIgnorePattern: '^_',
+          args: 'after-used',
+          argsIgnorePattern: '^_',
+        },
       ],
-      "no-unused-vars": "warn",
+      'no-duplicate-imports': 'error',
     },
   },
 ];
