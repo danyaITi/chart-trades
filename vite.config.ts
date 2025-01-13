4dd36da44d5f1e2c24e427234d55dfa7bfd0e73e
danyaITi <danil.lenskiy.1997@mail.ru>
2025-01-13 18:26:30 +0300

    added base alias paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/** @format */
 
-// https://vite.dev/config/
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@assets": path.resolve(__dirname, "./src/assets"),
+      "@components": path.resolve(__dirname, "./src/components"),
+    },
+  },
   plugins: [react()],
-})
+});
